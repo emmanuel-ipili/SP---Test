0eda3ff0d5c9790536b3fe42efaf9aebd9edf15a
Emmanuel Chua Ipili <emmanuel.ipili@gmail.com>
2019-06-03 05:26:35 +0800

    User Story:Provide API to retrive friends list given an email address
    1. Implement User Story 2
    2. Missing unit tests and java docs.

--- friend_management/src/main/java/com/sp/friend_management/controller/FriendController.java
+++ friend_management/src/main/java/com/sp/friend_management/controller/FriendController.java
@@ -1,17 +1,20 @@
 package com.sp.friend_management.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sp.friend_management.exception.EmailInvalidException;
 import com.sp.friend_management.exception.FriendRequestException;
 import com.sp.friend_management.exception.UserAccountNotFoundException;
 import com.sp.friend_management.model.Friend;
 import com.sp.friend_management.model.UserAccount;
 import com.sp.friend_management.repository.UserAccountRepository;
 import com.sp.friend_management.request.FriendRequest;
+import com.sp.friend_management.response.FriendListResponse;
 import com.sp.friend_management.response.FriendResponse;
 
 @RestController
@@ -45,7 +48,30 @@ public class FriendController {
 		
 		repository.save(userAccount1);
 		repository.save(userAccount2);
-		// return a json response with just the variable success that is a boolean
-	    return new FriendResponse(true);
+		 
+		return new FriendResponse(true);
+	  }
+	  
+	  @PostMapping("/friendlist")
+	  public FriendListResponse getFriends(@RequestBody FriendRequest request) {
+		String email = request.getEmail();
+		if (email.isEmpty()) {
+			//The checking here should not just be empty email but check whether the email is valid in terms of format.
+			throw new EmailInvalidException();
+		}
+		
+		UserAccount userAccount = repository.findById(email)
+	      .orElseThrow(() -> new UserAccountNotFoundException(email));
+		
+		FriendListResponse response = new FriendListResponse(true);
+		List<String> friends = new ArrayList<String>();
+	    for (Friend friend : userAccount.getFriends()) {
+	    	friends.add(friend.getFriendId());
+	    }
+		
+	    response.setFriends(friends);
+	    response.setCount(friends.size());
+	    
+		return response;
 	  }
 }
